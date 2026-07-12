de42eca238e3f71965c5d0ccdd39349ba5ef21e1
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:07 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,43 +1,38 @@
-output "security_center_automations" {
-  description = "All security_center_automation resources"
-  value       = azurerm_security_center_automation.security_center_automations
-  sensitive   = true
-}
 output "security_center_automations_action" {
-  description = "List of action values across all security_center_automations"
-  value       = [for k, v in azurerm_security_center_automation.security_center_automations : v.action]
+  description = "Map of action values across all security_center_automations, keyed the same as var.security_center_automations"
+  value       = { for k, v in azurerm_security_center_automation.security_center_automations : k => v.action }
   sensitive   = true
 }
 output "security_center_automations_description" {
-  description = "List of description values across all security_center_automations"
-  value       = [for k, v in azurerm_security_center_automation.security_center_automations : v.description]
+  description = "Map of description values across all security_center_automations, keyed the same as var.security_center_automations"
+  value       = { for k, v in azurerm_security_center_automation.security_center_automations : k => v.description }
 }
 output "security_center_automations_enabled" {
-  description = "List of enabled values across all security_center_automations"
-  value       = [for k, v in azurerm_security_center_automation.security_center_automations : v.enabled]
+  description = "Map of enabled values across all security_center_automations, keyed the same as var.security_center_automations"
+  value       = { for k, v in azurerm_security_center_automation.security_center_automations : k => v.enabled }
 }
 output "security_center_automations_location" {
-  description = "List of location values across all security_center_automations"
-  value       = [for k, v in azurerm_security_center_automation.security_center_automations : v.location]
+  description = "Map of location values across all security_center_automations, keyed the same as var.security_center_automations"
+  value       = { for k, v in azurerm_security_center_automation.security_center_automations : k => v.location }
 }
 output "security_center_automations_name" {
-  description = "List of name values across all security_center_automations"
-  value       = [for k, v in azurerm_security_center_automation.security_center_automations : v.name]
+  description = "Map of name values across all security_center_automations, keyed the same as var.security_center_automations"
+  value       = { for k, v in azurerm_security_center_automation.security_center_automations : k => v.name }
 }
 output "security_center_automations_resource_group_name" {
-  description = "List of resource_group_name values across all security_center_automations"
-  value       = [for k, v in azurerm_security_center_automation.security_center_automations : v.resource_group_name]
+  description = "Map of resource_group_name values across all security_center_automations, keyed the same as var.security_center_automations"
+  value       = { for k, v in azurerm_security_center_automation.security_center_automations : k => v.resource_group_name }
 }
 output "security_center_automations_scopes" {
-  description = "List of scopes values across all security_center_automations"
-  value       = [for k, v in azurerm_security_center_automation.security_center_automations : v.scopes]
+  description = "Map of scopes values across all security_center_automations, keyed the same as var.security_center_automations"
+  value       = { for k, v in azurerm_security_center_automation.security_center_automations : k => v.scopes }
 }
 output "security_center_automations_source" {
-  description = "List of source values across all security_center_automations"
-  value       = [for k, v in azurerm_security_center_automation.security_center_automations : v.source]
+  description = "Map of source values across all security_center_automations, keyed the same as var.security_center_automations"
+  value       = { for k, v in azurerm_security_center_automation.security_center_automations : k => v.source }
 }
 output "security_center_automations_tags" {
-  description = "List of tags values across all security_center_automations"
-  value       = [for k, v in azurerm_security_center_automation.security_center_automations : v.tags]
+  description = "Map of tags values across all security_center_automations, keyed the same as var.security_center_automations"
+  value       = { for k, v in azurerm_security_center_automation.security_center_automations : k => v.tags }
 }
 
